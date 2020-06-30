ca449f0f1e5fca43cd5f6abbb75357f34cf17c7b
Jeff Highsmith <junk@jeffhighsmith.com>
2020-06-30 14:45:30 -0400

    added comment to ino

--- PaintDispenser.ino
+++ PaintDispenser.ino
@@ -1,8 +1,12 @@
+// Paint dispenser controlled by Feather M0 Express, has two Sharp IR sensors.
+// Uses Pololu #3146 Jrk G2 18v19 Motor Controller.
+
 void setup() {
+
 
 }
 
 void loop() {
 
-    
+
 }
