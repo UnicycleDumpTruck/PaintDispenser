7b8421db414061921873090c2c179425ab7a8ba2
Jeff Highsmith <junk@jeffhighsmith.com>
2020-07-02 10:47:33 -0400

    palette must leave before new dispense

--- PaintDispenser.ino
+++ PaintDispenser.ino
@@ -7,14 +7,16 @@
 
 #define LEFT_SENSOR_PIN A0
 #define RIGHT_SENSOR_PIN A1
-#define LEFT_SENSOR_THRESHOLD 250
-#define RIGHT_SENSOR_THRESHOLD 250
+#define LEFT_SENSOR_THRESHOLD 200
+#define RIGHT_SENSOR_THRESHOLD 200
 #define DISPENSE_DURATION 1000
 #define DISPENSE_SPEED 1800
 #define STOP_SPEED 2048
 #define POST_DISPENSE_DELAY 1000
 
 JrkG2I2C jrk;
+
+bool palette_clear = true;
 
 bool detect(int sensor_pin, int threshold) {
   float measuredSensor = analogRead(sensor_pin);
@@ -43,7 +45,16 @@ void setup()
 void loop()
 {
   if (detect(LEFT_SENSOR_PIN, LEFT_SENSOR_THRESHOLD) && detect(RIGHT_SENSOR_PIN, RIGHT_SENSOR_THRESHOLD)) {
-    dispense();
+    if (palette_clear) {
+      palette_clear = false;
+      Serial.println("Palette appeared");
+      dispense();
+    } else { // Palette still there from previous dispense
+      Serial.println("hanging around");
+    } 
+  } else { // no palette present
+    palette_clear = true;
+    Serial.println("Palette gone");
   }
   delay(100);
 }
