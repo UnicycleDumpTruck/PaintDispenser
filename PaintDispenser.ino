d06a354b8c00af30f059c43416ce71beeee42efa
Jeff Highsmith <junk@jeffhighsmith.com>
2020-07-06 10:35:34 -0400

    added comments

--- PaintDispenser.ino
+++ PaintDispenser.ino
@@ -1,7 +1,10 @@
 // Paint dispenser controlled by Feather M0 Express, has two Sharp IR sensors.
 // https://learn.adafruit.com/adafruit-feather-m0-express-designed-for-circuit-python-circuitpython/using-with-arduino-ide
 
-// Uses Pololu #3146 Jrk G2 18v19 Motor Controller.
+// Pololu #3146 Jrk G2 18v19 Motor Controller connected via i2c, also using pins 5 and 6 for RST and ERR, not sure which.
+
+// NeoPixel LED strip connected to pin 9
+
 
 
 #include <JrkG2.h>
@@ -14,7 +17,7 @@
 #define DISPENSE_DURATION 1000
 #define DISPENSE_SPEED 1800
 #define STOP_SPEED 2048
-#define POST_DISPENSE_DELAY 0
+#define POST_DISPENSE_DELAY 0 // Not used, as lights must 'unfill', functioning as delay
 
 #define LED_PIN 9
 #define LED_COUNT 13 // 13 leds on prototype housing
@@ -36,7 +39,7 @@ JrkG2I2C jrk; // Pololu motor controller
 bool palette_clear = false;
 bool not_dispensed = true;
 
-bool detect(int sensor_pin, int threshold) {
+bool detect(int sensor_pin, int threshold) { // Read a sensor, return true if it's over the threshold
   float measuredSensor = analogRead(sensor_pin);
   Serial.println(measuredSensor);
   if (measuredSensor > threshold) {
@@ -45,7 +48,7 @@ bool detect(int sensor_pin, int threshold) {
   return false;
 }
 
-void dispense() {
+void dispense() { // Run the motor at DISPENSE_SPEED for DISPENSE_DURATION
   Serial.println("Dispensing");
   jrk.setTarget(DISPENSE_SPEED);
   delay(DISPENSE_DURATION);
@@ -53,49 +56,46 @@ void dispense() {
   delay(POST_DISPENSE_DELAY);
 }
 
-void ledGreen() {
+void ledGreen() { // Light strip full green, no delays
   for (int i=LED_COUNT; i>=0; i--) {
     strip.setPixelColor(i, 0, PIXEL_BRIGHTNESS, 0);
     strip.show();
-    //delay(FILL_DELAY);
   }
 }
 
-void ledRed() {
+void ledRed() { // Light strip full red, no delays
   for (int i=0; i<LED_COUNT; i++) {
     strip.setPixelColor(i, PIXEL_BRIGHTNESS, 0, 0);
     strip.show();
-    //delay(FILL_DELAY);
   }
 }
 
 void setup()
 {
-  // Set up I2C.
-  Wire.begin();
-  Serial.begin(115200);
-  strip.begin();
+  Wire.begin(); // Set up I2C.
+  Serial.begin(115200); // Commence Serial communication
+  strip.begin(); // Start the NeoPixel strip
   ledGreen();
 }
 
 void loop()
 {
   
-  if ((led_goal == SOLID_GREEN) && red_leader > -1) {
-    if ((millis() - prev_led_change_millis) > UNFILL_DELAY) {
+  if ((led_goal == SOLID_GREEN) && red_leader > -1) { // If our goal is green, but we aren't fully green yet
+    if ((millis() - prev_led_change_millis) > UNFILL_DELAY) { // if we have waited long enough since the last led change
       strip.setPixelColor(red_leader, 0, PIXEL_BRIGHTNESS, 0);
       strip.show();
-      red_leader--;
+      red_leader--; // Decrement after led change
       prev_led_change_millis = millis();
     }
-  } else if ((led_goal == SOLID_RED) && red_leader < LED_COUNT) {
-    if ((millis() - prev_led_change_millis) > FILL_DELAY) {
-      red_leader++;
+  } else if ((led_goal == SOLID_RED) && red_leader < LED_COUNT) { // if our goal is red, but we aren't fully red yet
+    if ((millis() - prev_led_change_millis) > FILL_DELAY) { // if we have waited long enough since the last led change
+      red_leader++; // Increment before led change
       strip.setPixelColor(red_leader, PIXEL_BRIGHTNESS, 0, 0);
       strip.show();
       prev_led_change_millis = millis();
     }
-  }
+  } // else we have achieved our led goal
   
 
   if (detect(LEFT_SENSOR_PIN, LEFT_SENSOR_THRESHOLD) && detect(RIGHT_SENSOR_PIN, RIGHT_SENSOR_THRESHOLD)) { // palette now present
@@ -120,5 +120,5 @@ void loop()
     Serial.println("Palette gone");
     led_goal = SOLID_GREEN;
   }
-  delay(5);
+  delay(5); // we don't need it running too fast
 }
